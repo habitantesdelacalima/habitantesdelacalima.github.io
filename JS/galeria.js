21c3165971014b0b1030514203861abfa2367436
bryzac <habitantesdelacalima@gmail.com>
2022-10-28 01:16:39 -0300

    Galería
    Galería para Paisajes de Calima lista

---
+++ JS/galeria.js
@@ -0,0 +1,11 @@
+const imgBigBox = document.getElementById("img_big_box"),
+imgBig = document.getElementById("img_big");
+
+function openFullImg(reference){
+    imgBigBox.style.display = "flex";
+    imgBig.src = reference;
+}
+
+function closeImg(){
+    imgBigBox.style.display = "none";
+}
