c3075b777967dcfe60b6bca6f6873e178dc74737
bryzac <habitantesdelacalima@gmail.com>
2022-11-09 15:47:13 -0300

    4
    65465

--- JS/cenizaskl.js
+++ JS/cenizaskl.js
@@ -1,30 +1,30 @@
-const about = document.querySelector('body')
+const about = document.querySelector('body');
 
 const crearCenizaskl = () => {
-    let pavesakl = document.createElement('j')
-    let x = innerWidth * Math.random()
-    let size = (Math.random() * 4) + 2
-    let delay = Math.random() * 5
-    let anima = (Math.random() * 15) 
-    let z = Math.round(Math.random()) * 100
-    let grado = Math.random() * 90
-    let derecha = (innerWidth * Math.random()) * 0.4
+    let pavesakl = document.createElement('j');
+    let x = innerWidth * Math.random();
+    let size = (Math.random() * 4) + 2;
+    let delay = Math.random() * 5;
+    let anima = (Math.random() * 15) ;
+    let z = Math.round(Math.random()) * 100;
+    let grado = Math.random() * 90;
+    let derecha = (innerWidth * Math.random()) * 0.4;
 
 
-    pavesakl.style.left = (x - derecha) + 'px'
-    pavesakl.style.width = (size * 2) + 'px'
-    pavesakl.style.height = size + 'px'
-    pavesakl.style.animationDelay = delay + 's'
-    pavesakl.style.animationDuration = anima + 's'
-    pavesakl.style.zIndex = z
-    pavesakl.style.rotate = grado + 'deg'
+    pavesakl.style.left = (x - derecha) + 'px';
+    pavesakl.style.width = (size * 2) + 'px';
+    pavesakl.style.height = size + 'px';
+    pavesakl.style.animationDelay = delay + 's';
+    pavesakl.style.animationDuration = anima + 's';
+    pavesakl.style.zIndex = z;
+    pavesakl.style.rotate = grado + 'deg';
     
 
-    body.appendChild(pavesakl)
+    body.appendChild(pavesakl);
 
     setTimeout(() => {
         pavesakl.remove()
-    }, anima * 1000)}
+    }, anima * 1000)};
 
 
-setInterval(crearCenizaskl, 200)
+setInterval(crearCenizaskl, 200);
