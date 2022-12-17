5055f2014e355289704ce018e38f4520b66a875f
bryzac <habitantesdelacalima@gmail.com>
2022-12-17 19:16:35 -0300

    sadf
    asfaFas

--- JS/poemario.js
+++ JS/poemario.js
@@ -1,10 +1,12 @@
+var url = '/Libros/Sequía.pdf';
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.2.2/pdf.worker.js';
+
+
 const centrar = document.querySelector('#canvases');
 centrar.classList.add('center');
 centrar.innerHTML = '<span class="loader"></span>';
 
-var url = '/Libros/Sequía.pdf';
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.2.2/pdf.worker.js';
 
 var pdfDoc = null,
     pageNum = 1,
