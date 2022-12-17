5064a534a453cbe83462408262ae9c57ae20598b
bryzac <habitantesdelacalima@gmail.com>
2022-12-17 14:15:47 -0300

    aa
    aa

--- JS/poemario.js
+++ JS/poemario.js
@@ -1,5 +1,6 @@
 const centrar = document.querySelector('#canvases');
 centrar.classList.add('center');
+centrar.innerHTML = '<span class="loader"></span>'
 
 var url = '/Libros/Sequía.pdf';
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
