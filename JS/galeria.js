adbe028608d2b8e4d3fe6af5180ccbb74643b924
bryzac <habitantesdelacalima@gmail.com>
2022-12-23 23:08:28 -0300

    asdad

--- JS/galeria.js
+++ JS/galeria.js
@@ -1,6 +1,7 @@
 const imgBigBox = document.getElementById("img_big_box"),
 imgBig = document.getElementById('img_big');
 
+
 function openFullImg(reference){
     imgBigBox.style.display = "flex";
     imgBig.src = reference;
